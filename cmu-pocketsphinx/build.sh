95ee0fd890e36b65e34c91f09d3b5c1c8bcd4eaf
johannes <johannes@johannes-laptop.(none)>
2014-09-07 00:34:04 +0200

    add pkgconfig path

--- cmu-pocketsphinx/build.sh
+++ cmu-pocketsphinx/build.sh
@@ -3,6 +3,7 @@
 # build dependencies:
 # sudo apt-get install build-essential bison
 
+export PKG_CONFIG_PATH=$PREFIX/cmu-sphinx/lib/pkgconfig
 ./configure --prefix=$PREFIX/cmu-sphinx
 make clean all
 make test
