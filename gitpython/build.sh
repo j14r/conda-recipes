aaf325ea0b1d8b8abc9b85a21b30542e2f38c6b3
johannes <johannes@example.com>
2015-04-03 21:33:45 +0200

    add some recipes

--- gitpython/build.sh
+++ gitpython/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-mkdir -vp ${PREFIX}/bin;
+$PYTHON setup.py install
 
-${PYTHON} setup.py install || exit 1;
+# Add more build steps here, if they are necessary.
 
+# See
+# http://docs.continuum.io/conda/build.html
+# for a list of environment variables that are set during the build process.
